427ce1e26c29d2246005a5e7e5dfaef3253fe35f
melody-ste <melody_stephan@icloud.com>
2025-09-20 23:55:07 +0200

    switching the providers from app to main

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,24 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import { BrowserRouter } from "react-router-dom";
+import { IntlProvider } from "react-intl";
+import { messages } from "./i18n/messages";
+import { ThemeProvider } from "./context/ThemeContext.jsx";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+
+const locale = navigator.language.startsWith("fr") ? "fr" : "en";
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <IntlProvider locale={locale} messages={messages[locale]}>
+      <ThemeProvider>
+        <BrowserRouter>
+          <App />
+          <ToastContainer />
+        </BrowserRouter>
+      </ThemeProvider>
+    </IntlProvider>
   </StrictMode>,
 )
