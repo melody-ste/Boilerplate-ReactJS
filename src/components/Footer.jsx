390839562daa5eb1f764727f332b2ca3cd6490fd
melody-ste <melody_stephan@icloud.com>
2025-09-20 21:45:55 +0200

    adding navbar/footer in component + basic routes in app

---
+++ src/components/Footer.jsx
@@ -0,0 +1,15 @@
+import { Link } from "react-router-dom";
+
+const Footer = () => {
+  return (
+    <footer>
+      <ul>
+        <li>
+          Footer
+        </li>
+      </ul>
+    </footer>
+  );
+};
+
+export default Footer;
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,13 @@
+import { Link } from "react-router-dom";
+
+const Navbar = () => {
+  return (
+    <nav>
+      <ul>
+        <li>Navbar</li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Navbar;
